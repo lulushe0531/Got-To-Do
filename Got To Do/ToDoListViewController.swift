db4aec6a133fd8c50ad9ba414546dc9adea7978a
LuLu <shelu428@gmail.com>
2018-04-12 12:14:01 -0700

    Added readme

--- Got To Do/ToDoListViewController.swift
+++ Got To Do/ToDoListViewController.swift
@@ -11,8 +11,14 @@ import UIKit
 class ToDoListViewController: UITableViewController {
 
     var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
         
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -50,6 +56,7 @@ class ToDoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add", style: .default) { (action) in
             self.itemArray.append(textField.text!)
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             self.tableView.reloadData()
         }
         
---
+++ readme.md
@@ -0,0 +1,3 @@
+This is an IOS app using swift4.
+
+
